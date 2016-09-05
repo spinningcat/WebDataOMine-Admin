a6ecf86ba0ae76ddc66a5ab49e0a32a667aa680a
Emrah Oner <emrahoner@gmail.com>
2016-09-05 20:23:18 +0300

    Flask module added

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from django.http import HttpResponse
+from flask import Flask
+app = Flask(__name__)
 
-def index(request):
-    return HttpResponse("WDOM Admin")
+@app.route("/")
+def index():
+    return "<h1>Web Data O'Mine Project<h1><h2>Admin Site<h2>"
+
+if __name__ == "__main__":
+    app.run()
