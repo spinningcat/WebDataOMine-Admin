183ebf2e480003d4ffebfa1b1a4ba69debe2aa93
Emrah Oner <emrahoner@gmail.com>
2016-09-06 00:16:05 +0300

    fix

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import sys
+
+devMode = len(sys.argv) > 1 and sys.argv[1] == "dev"
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1>Web Data O'Mine Project<h1><h2>Admin Site<h2>"
 
+
 if __name__ == "__main__":
-    app.run()
+    if devMode:
+        app.run(port=8080)
+    else:
+        app.run(port=80)
