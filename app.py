d4324277caf6246d7ab5e4df4eeba3d7834acf89
Emrah Oner <emrahoner@gmail.com>
2016-09-06 08:13:47 +0300

    application port setting

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Web Data O'Mine Project<h1><h2>Admin Site<h2>"
+    return "<h1>Web Data O'Mine<h1><h2>Admin Project<h2>"
 
 
 if __name__ == "__main__":
